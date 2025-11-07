a2e8b3343b4348039163f79935ba870220fa618e
LuanK63 <luannguyen.091104@gmail.com>
2025-11-07 15:15:52 +0700

    feat(backend/controllers): thêm controllers Jobs, News, Products, Articel

---
+++ WebJob/Controllers/JobsController.cs
@@ -0,0 +1,174 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebJob.Models;
+
+namespace WebJob.Controllers
+{
+    public class JobsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        
+        public ActionResult Index(int? id, int? page)
+        {
+            // Bắt đầu với danh sách công việc đang hoạt động
+            var query = db.Jobs.Where(x => x.IsActive);
+
+            // Nếu có id hợp lệ, lọc theo danh mục công việc
+            if (id > 0)
+            {
+                query = query.Where(x => x.JobCategoryID == id);
+            }
+
+            // Kiểm tra nếu không có công việc nào trong query
+            if (!query.Any())
+            {
+                ViewBag.NoJobs = "Không có công việc nào được lưu bởi tài khoản này.";
+            }
+
+            // Thiết lập phân trang
+            var pageNumber = page ?? 1; // Trang hiện tại
+            var pageSize = 5;           // Số mục trên mỗi trang
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = pageNumber;
+
+            // Chuyển danh sách query thành dạng phân trang với thứ tự mới nhất
+            var pagedList = query.OrderByDescending(x => x.CreatedDate).ToPagedList(pageNumber, pageSize);
+
+            // Cung cấp thông tin danh mục cho view
+            ViewBag.CateId = id;
+
+            return View(pagedList);
+        }
+
+
+
+        public ActionResult Detail(string alias, int id)
+        {
+            var item = db.Jobs.Find(id);
+
+            // tang luot xem khi click vao
+            if(item != null)
+            {
+                db.Jobs.Attach(item);
+                item.ViewCount = item.ViewCount + 1;
+                db.Entry(item).Property(x => x.ViewCount).IsModified = true;
+                db.SaveChanges();
+            }
+
+           
+
+            // lấy công việc tương tự
+            return View(item);
+        }
+
+        public ActionResult JobCategory(string alias, int id)
+        {
+            // Bắt đầu với danh sách công việc đang hoạt động
+            var query = db.Jobs.Where(x => x.IsActive);
+
+            // Nếu có id hợp lệ, lọc theo danh mục công việc
+            if (id > 0)
+            {
+                query = query.Where(x => x.JobCategoryID == id);
+            }
+
+            // Chuyển đổi query thành danh sách
+            var items = query.ToList();
+
+            // Cung cấp thông tin danh mục cho view
+            ViewBag.CateId = id;
+            ViewBag.JobCount = items.Count;
+            return View(items);
+        }
+
+
+        public ActionResult View_itemsByCateId(string alias, int? id)
+        {
+            // Tìm kiếm danh mục công việc dựa trên alias
+            var category = db.JobCategories.FirstOrDefault(c => c.Alias == alias);
+            if (category == null)
+            {
+                return HttpNotFound(); 
+            }
+
+            // Nếu id không phải null và khớp với danh mục công việc, tìm kiếm các công việc
+            if (id.HasValue && id.Value == category.JobCategoryID && category.IsActive)
+            {
+                var items = db.Jobs.Where(x => x.JobCategoryID == category.JobCategoryID && x.IsActive).ToList();
+                ViewBag.JobCount = items.Count;
+                return View(items); 
+            }
+
+            return HttpNotFound(); 
+        }
+
+
+   
+        public ActionResult Partial_JobNow()
+        {
+            var items = db.Jobs
+                .Where(x => x.IsActive && x.IsNow)
+                .GroupBy(x => x.Company.CompanyName) // Nhóm theo tên công ty
+                .Select(g => g.OrderBy(_ => Guid.NewGuid()).FirstOrDefault()) // Lấy công việc ngẫu nhiên trong nhóm
+                .OrderBy(_ => Guid.NewGuid()) // Sắp xếp ngẫu nhiên các nhóm
+                .Take(7) // Lấy tối đa 7 công việc
+                .ToList();
+
+            return PartialView(items);
+        }
+
+
+        // load việc làm mới nhất
+        public ActionResult Partial_JobLatest()
+        {
+            // Lấy danh sách 10 công việc mới nhất dựa trên ngày tạo (CreatedDate)
+            var items = db.Jobs
+                          .Where(x => x.IsActive)
+                          .OrderByDescending(x => x.CreatedDate) // Sắp xếp theo ngày tạo mới nhất
+                          .Take(10) // Giới hạn 10 công việc
+                          .ToList();
+
+            return PartialView(items); // Trả về PartialView với danh sách công việc
+        }
+  
+
+        public ActionResult Partial_CompanyLogos()
+        {
+            var companies = db.CompanyImages
+                              .Where(x => x.IsDefault)
+                              .GroupBy(c => c.Company.CompanyName) // Nhóm theo tên công ty
+                              .Select(g => g.FirstOrDefault())// Lấy công ty đầu tiên trong nhóm
+                              .Take(8)
+                              .ToList();
+            return PartialView(companies);
+        }
+
+
+
+        public ActionResult Partial_HighSalaryJobs()
+        {
+            // Lọc các công việc có mức lương > 10 triệu (10,000,000)
+            var highSalaryJobs = db.Jobs.Where(x =>x.Salary.SalaryMax > 10000000 && x.IsActive).ToList();
+
+            return PartialView(highSalaryJobs);
+        }
+        public PartialViewResult RelatedJobs(string categoryName, int excludeJobId)
+        {
+            // Lấy danh sách việc làm tương tự từ database
+            var relatedJobs = db.Jobs
+                                .Where(x => x.JobCategory.CategoryName == categoryName && x.JobID != excludeJobId)
+                                .OrderByDescending(x => x.CreatedDate)
+                                .Take(5) // Lấy 5 việc làm mới nhất
+                                .ToList();
+
+            return PartialView("_RelatedJobs", relatedJobs);
+        }
+
+
+    }
+}
---
+++ WebJob/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebJob.Models;
+
+namespace WebJob.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Jobs
+        // Phương thức Index để hiển thị danh sách sản phẩm
+        public ActionResult Index(int? id, int page = 1)
+        {
+            var products = db.products.AsQueryable();
+
+            // Lọc sản phẩm theo danh mục nếu có id
+            if (id != null)
+            {
+                products = products.Where(x => x.CateProId == id);
+            }
+
+            // Chuyển đổi thành IPagedList với số lượng sản phẩm trên mỗi trang (3 sản phẩm/trang)
+            var pagedProducts = products.OrderBy(x => x.ProductID).ToPagedList(page, 3);
+
+            // Truyền dữ liệu vào View
+            ViewBag.CategoryId = id;  // Để giữ lại giá trị danh mục đã chọn khi phân trang
+            return View(pagedProducts);
+        }
+
+
+        public ActionResult DetailProduct(string alias, int id)
+        {
+
+            var item = db.products.Find(id);
+            var defaultImages = item.ProductImages.Where(x => x.IsDefault).ToList();
+            ViewBag.DefaultImages = defaultImages;
+            return View(item);
+        }
+
+        public ActionResult ProductCategory(string alias, int id)
+        {
+            var items = db.products.ToList();
+            if (id > 0)
+            {
+                items = items.Where(x => x.CateProId == id).ToList();
+            }
+
+            ViewBag.CateId = id;
+            return View(items);
+        }
+
+        public ActionResult View_itemsByCatePdId(int page = 1, int pageSize = 5)
+        {
+            var items = db.products.Where(x => x.IsActive).OrderBy(x => x.ProductID).ToPagedList(page, pageSize);
+            return PartialView(items);
+        }
+
+
+    }
+}
